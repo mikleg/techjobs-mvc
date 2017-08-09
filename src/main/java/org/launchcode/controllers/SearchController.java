ea4b3f97313e305f36ca4eb3d8eafc3978993c38
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-08-08 21:46:25 -0700

    a bug search-search was fixed

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -16,7 +17,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
+    private String lastChoise = "core competency";
 
     // TODO #1 - Create handler to process search request and display results
     @RequestMapping(value = "")
@@ -27,24 +28,32 @@ public class SearchController {
     }
 
     @RequestMapping(value = "results")
-    public String listColumnValues(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    public String listColumnValues(Model model, @RequestParam Optional<String> searchType, @RequestParam String searchTerm) {
             ArrayList<HashMap<String, String>> items = new ArrayList<>();
+            String aSearchType = "";
 
-        if (!searchType.equals("all"))
-            {
-                items = JobData.findByColumnAndValue(searchType, searchTerm);
+            StringBuilder rrrr = new StringBuilder("");
+            searchType.ifPresent(value -> rrrr.append(value));
+
+
+            if (searchType.isPresent()){
+                aSearchType += rrrr.toString();
             }
-            else
-            {
-                items = JobData.findByValue(searchTerm);
+            else {
+                aSearchType = lastChoise;
             }
+                if (!aSearchType.equals("all")) {
+                    items = JobData.findByColumnAndValue(aSearchType, searchTerm);
+                } else {
+                    items = JobData.findByValue(searchTerm);
+                }
+                lastChoise = aSearchType;
 
-
-            model.addAttribute("title", "All " + ListController.columnChoices.get(searchType) + " Values");
-            model.addAttribute("column", searchType);
-            model.addAttribute("items", items);
-            model.addAttribute("par1", searchType);
-            model.addAttribute("par2", searchTerm);
+                model.addAttribute("title", "All " + ListController.columnChoices.get(aSearchType) + " Values");
+                model.addAttribute("column", aSearchType);
+                model.addAttribute("items", items);
+                /*model.addAttribute("par1", aSearchType);
+                model.addAttribute("par2", searchTerm);*/
 
             return "search";
 
